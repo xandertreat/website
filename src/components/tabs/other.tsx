c746de3f57fb7d8d135dcea9c6934e11fc050cc3
Xander Treat <xtreat@asu.edu>
2025-06-14 15:54:55 -0700

    more styling improvements

--- src/components/tabs/other.tsx
+++ src/components/tabs/other.tsx
@@ -2,8 +2,8 @@ export default function Other() {
 	return (
 		<p class="font-extralight text-neutral opacity-90">
 			this is where I'll put everything cool & <br />
-			interesting that isn't directly relevant to the site! <br /> nothing here
-			(<strong>yet!</strong>)
+			interesting that isn't directly relevant to the site <br /> nothing here (
+			<strong>yet!</strong>)
 		</p>
 	);
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,9 +11,12 @@ export default function Index() {
 		<div class="motion-duration-3000 motion-opacity-in mx-auto flex h-screen w-1/2 items-center justify-center text-center">
 			<main class="flex flex-col items-center justify-center gap-3 lg:block">
 				<span class="inline-flex items-center gap-8">
-					<h1 class="relative font-semibold text-3xl">
-						Xander Treat{" "}
-						<AppThemeSwitcher class="tooltip tooltip-info btn btn-ghost btn-circle motion-duration-500 motion-ease-in-out motion-delay-600 -top-1 -right-5.5 absolute size-5" />
+					<h1 class="motion-preset-slide-down relative font-semibold text-3xl">
+						Xander Treat
+						<AppThemeSwitcher
+							class="tooltip tooltip-info btn btn-ghost btn-circle motion-duration-500 motion-ease-in-out motion-delay-600 -top-1 -right-5.5 absolute size-5 lg:grayscale"
+							onMouseEnter={(e) => e.target.classList.remove("lg:grayscale")}
+						/>
 					</h1>
 					<Links class="lg:hidden" />
 				</span>
