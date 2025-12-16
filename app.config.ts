3d61adc2228016d3c44c30808b592bd57babc18e
Xander Treat <xandertreat@proton.me>
2025-12-15 19:14:04 -0700

    ssr off
    after react it just isn't worth it

--- app.config.ts
+++ app.config.ts
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+	ssr: false,
 	server: {
 		node: true,
 		preset: 'node-server',
